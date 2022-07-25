f145a1f4dc5c9bf8f82b12d658cfd042ed0f1c1f
Muhamad Islahuddin <el.islah.udin@gmail.com>
2022-07-25 08:14:29 +0700

    add cords

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express()
 const httpServer = createServer(app)
 const io = new Server(httpServer, {
     cors : {
-        origin : 'http://localhost:3000'
+        origin : ['http://localhost:3000','https://telegram-xi.vercel.app']
     }
 })
 app.use(express.json())
